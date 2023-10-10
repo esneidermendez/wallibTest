1502240e78fbc691a2b18ec73092db8f673a59c2
esneidermendez <esneidermendez@soyyo.co>
2023-10-09 20:13:28 -0500

    Add test for Wallib

---
+++ src/Circle.java
@@ -0,0 +1,7 @@
+public class Circle implements Form {
+    @Override
+    public void draw(){
+        System.out.println("Drawing circle");
+    }
+}
+
---
+++ src/ShapeMaker.java
@@ -0,0 +1,14 @@
+public class ShapeMaker {
+    public void drawCircle(){
+        Circle circle = new Circle();
+        circle.draw();
+    }
+    public void drawRectangle(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.draw();
+    }
+    public void drawSquare(){
+        Square square = new Square();
+        square.draw();
+    }
+}
